d9b0748f96a8ee40e1abb8b74a7b753b20c60006
jujusoi <jalxmcdonald@hotmail.com>
2023-10-18 15:33:33 +1030

    added working db route i think

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT into the db');
+  const jateDB = await open('jate', 1);
+  const ts = jateDB.transaction('jate', 'readwrite');
+  const store = ts.objectStore('jate');
+  const request = store.put({ test: content });
+  const result = await request;
+  console.log(`PUT result: ${result}`);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.log('GET all from db');
+  const jateDB = await open('jate', 1);
+  const ts = jateDB.transaction('jate', 'readonly');
+  const store = ts.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log(result);
+  return result;
+}
 initdb();
